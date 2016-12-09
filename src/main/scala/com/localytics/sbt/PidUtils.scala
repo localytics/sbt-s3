c8a24d0ba74691304d3711620ca58ec0b4d9c438
Benjamin Darfler <bdarfler@gmail.com>
2016-12-09 16:50:20 -0500

    Breakout PidUtils

---
+++ src/main/scala/com/localytics/sbt/PidUtils.scala
@@ -0,0 +1,17 @@
+package com.localytics.sbt
+
+object PidUtils {
+
+  private val ProcessIDRegex = """\d+ .*s3proxy""".r
+
+  def extractS3ProxyPid(input: String): Option[String] = ProcessIDRegex.findFirstIn(input).map(_.split(" ")(0))
+
+  def osName: String = System.getProperty("os.name") match {
+    case n: String if !n.isEmpty => n
+    case _ => System.getProperty("os")
+  }
+
+  def killPidCommand(pid: String): String =
+    if (osName.toLowerCase.contains("windows")) s"Taskkill /PID $pid /F" else s"kill $pid"
+
+}
---
+++ src/test/scala/com/localytics/sbt/PidUtilsTest.scala
@@ -0,0 +1,23 @@
+package com.localytics.sbt
+
+import org.scalatest.FunSpec
+import org.scalatest.Matchers
+
+class PidUtilsTest extends FunSpec with Matchers {
+
+  describe("PidUtils") {
+
+    it("should extract PID correctly") {
+      val jpsOutput =
+        """
+          |76224 /usr/local/Cellar/sbt/0.13.13/libexec/sbt-launch.jar
+          |76656 sun.tools.jps.Jps
+          |72451
+          |76364 /Users/person/code/repository/aws-mocks/s3/s3proxy-1.5.1
+        """.stripMargin
+      PidUtils.extractS3ProxyPid(jpsOutput) should equal(Some("76364"))
+    }
+
+  }
+
+}
