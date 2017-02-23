787de60769e87a0f0de12d2a6fb4cd36d3091d1a
Benjamin Darfler <bdarfler@gmail.com>
2017-02-23 14:18:31 -0500

    Create lazy val for settings

--- src/main/scala/com/localytics/sbt/s3/S3ProxyKeys.scala
+++ src/main/scala/com/localytics/sbt/s3/S3ProxyKeys.scala
@@ -2,6 +2,7 @@ package com.localytics.sbt.s3
 
 import java.io.File
 
+import sbt.Keys._
 import sbt._
 
 object S3ProxyKeys {
@@ -28,4 +29,24 @@ object S3ProxyKeys {
   lazy val startS3Proxy = TaskKey[String]("start-s3-proxy")
   lazy val stopS3Proxy = TaskKey[Unit]("stop-s3-proxy")
   lazy val s3ProxyTestCleanup = TaskKey[Tests.Cleanup]("s3-proxy-test-cleanup")
+
+  // http://www.scala-sbt.org/0.13/docs/Plugins-Best-Practices.html#Provide+raw+settings+and+configured+settings
+  lazy val baseS3ProxySettings = Seq(
+    s3ProxyVersion := "1.5.1",
+    s3ProxyDownloadDir := file("s3-proxy"),
+    s3ProxyDownloadUrl := s"https://github.com/andrewgaul/s3proxy/releases/download/s3proxy-${s3ProxyVersion.value}/s3proxy",
+    s3ProxyDownloadFile := s"s3proxy-${s3ProxyVersion.value}",
+    s3ProxyPort := 8001,
+    s3ProxyHeapSize := None,
+    s3ProxyDataDir := "s3-proxy/data",
+    s3ProxyCleanAfterStop := true,
+    s3ProxyAuthorization := NoAuth,
+    s3ProxyKeyStore := None,
+
+    downloadS3Proxy := DownloadS3Proxy(s3ProxyVersion.value, s3ProxyDownloadUrl.value, s3ProxyDownloadDir.value, s3ProxyDownloadFile.value, streams.value),
+    startS3Proxy := StartS3Proxy(s3ProxyDownloadDir.value, s3ProxyDownloadFile.value, s3ProxyPort.value, s3ProxyHeapSize.value, s3ProxyDataDir.value, s3ProxyAuthorization.value, s3ProxyKeyStore.value, streams.value),
+    stopS3Proxy := StopS3Proxy(streams.value, s3ProxyDataDir.value, s3ProxyCleanAfterStop.value),
+    s3ProxyTestCleanup := Tests.Cleanup(() => StopS3Proxy(streams.value, s3ProxyDataDir.value, s3ProxyCleanAfterStop.value)),
+    startS3Proxy := startS3Proxy.dependsOn(downloadS3Proxy).value
+  )
 }
